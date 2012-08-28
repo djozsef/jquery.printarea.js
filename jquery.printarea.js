017cec1722bb8a3b8c7a35af8d27a5a5c05cab6f
joe <joe@chilicreative.hu>
2012-08-28 13:01:34 +0200

    Added fix for the "media is undefined" error that affected code using jQuery version > 1.6.x. The problem was that $ returns undefined for non-existent attributes above 1.6.x. Added a typeof check for undefined media attribute.

--- jquery.printarea.js
+++ jquery.printarea.js
@@ -93,7 +93,7 @@
                 })
             .filter(function(){ // this filter contributed by "mindinquiring"
                     var media = $(this).attr("media");
-                    return (media.toLowerCase() == "" || media.toLowerCase() == "print")
+                    return (typeof media == "undefined" || media.toLowerCase() == "" || media.toLowerCase() == "print");
                 })
             .each(function(){
                     head += '<link type="text/css" rel="stylesheet" href="' + $(this).attr("href") + '" >';
